01b217d924a1d4d396fa682a4f5a3f47d6349acd
albertiq <nequeyjd2009@yandex.ru>
2022-03-22 23:21:31 +0500

    added test privat api + cbr api

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+import privat_api
 import test_api
 import models
 
@@ -17,6 +18,17 @@ class Test(unittest.TestCase):
         updated_after = xrate.updated
 
         self.assertEqual(xrate.rate, 1.01)
+        self.assertGreater(updated_after, updated_before)
+
+    def test_privat(self):
+        xrate = models.XRate.get(id=1)
+        updated_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+        privat_api.update_xrates(840, 980)
+        xrate = models.XRate.get(id=1)
+        updated_after = xrate.updated
+
+        self.assertGreater(xrate.rate, 25)
         self.assertGreater(updated_after, updated_before)
 
 
